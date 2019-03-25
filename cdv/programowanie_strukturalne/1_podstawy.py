ece97d560e92fa0595a412b06de161795f88cd1d
Krzysztof Dorożała <kdorozala@edu.cdv.pl>
2019-03-25 12:53:39 +0100

    python

---
+++ cdv/programowanie_strukturalne/1_podstawy.py
@@ -0,0 +1,34 @@
+print("cdv")
+print(2)
+
+#potega
+potega = 2 ** 10
+print(potega)
+
+tekst = "CDV"
+print(tekst * 2)
+
+#pobieranie danych z klawiatury
+imie = input()
+print("Twoje imie podane z klawiatury: " + imie)
+
+nazwisko = input()
+print("Twoje imie: " + imie + ", Twoje nazwisko: " + nazwisko)
+
+dlugosc = len(nazwisko)
+print(type(nazwisko)) #string
+print(type(dlugosc))  #int
+print("Dlugosc nazwiska: " ,dlugosc)
+dlugosc =str(dlugosc) #konwersja typu danych
+print("Dlugosc nazwiska: " + dlugosc)
+
+#uzytkownik wpisuje z klawiatury swoj wiek, wyswietl dane w formacie: Twoj wiek: ... lat
+
+print("\nPodaj swoj wiek: ", end="")
+wiek = input()
+print("Twoj wiek: " + wiek + " lat")
+
+
+
+
+print()
